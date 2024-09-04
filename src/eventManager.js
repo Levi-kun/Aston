be0a56857d0d350d941488272984b9bfd1e892c0
Levi-kun <p.joshua@outlook.com>
2024-09-03 20:05:01 -0400

    you know it happens

--- src/eventManager.js
+++ src/eventManager.js
@@ -1,15 +1,10 @@
-// eventManager.js
+
 const { EventEmitter } = require('events');
 
-// Create an event emitter
 const eventEmitter = new EventEmitter();
 
-// Define the custom event
 eventEmitter.on('spawnInCard', (cardData) => {
-    // Your logic for spawning a card goes here
     console.log('Card spawned:', cardData);
-    // You can emit additional events or perform other actions related to card spawning
 });
 
-// Export the event emitter
 module.exports = eventEmitter;
