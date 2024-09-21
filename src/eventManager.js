6d1c4a11a52620a9745425128748dc36cee7bb03
Levi-kun <p.joshua@outlook.com>
2024-09-21 19:15:03 -0400

    Changed rarity dk

--- src/eventManager.js
+++ src/eventManager.js
@@ -3,7 +3,7 @@ const { EventEmitter } = require("events");
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on("spawnInCard", (guild) => {
-    console.log("in guild:", guild.name);
+    return;
 });
 
 module.exports = eventEmitter;
