babae149aa8ede7b518e068eee24ab1cdf66a2d7
Levi-kun <p.joshua@outlook.com>
2024-09-21 19:22:34 -0400

    uh...

--- src/eventManager.js
+++ src/eventManager.js
@@ -3,7 +3,6 @@ const { EventEmitter } = require("events");
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on("spawnInCard", (guild) => {
-    return;
 });
 
 module.exports = eventEmitter;
