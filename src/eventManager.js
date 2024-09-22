ca9292dc8816c69aaf31976ff216db19637dbb4d
Levi-kun <p.joshua@outlook.com>
2024-09-22 15:56:20 -0400

    refrence added.

--- src/eventManager.js
+++ src/eventManager.js
@@ -3,6 +3,7 @@ const { EventEmitter } = require("events");
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on("spawnInCard", (guild) => {
+    console.log("in guild:", guild.name);
 });
 
 module.exports = eventEmitter;
